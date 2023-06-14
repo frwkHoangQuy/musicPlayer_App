1c261427d17e223690afd683eb23768c244dcc15
Quy <frwk.HoangQuy@gmail.com>
2023-06-15 03:22:50 +0700

    Controller and Library compeleted

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const {engine} = require("express-handlebars");
 const path = require('path')
 const app = express(); 
 const port = 3000; 
+app.use(express.static(path.join(__dirname, 'src', 'public')));
 app.set('view engine', 'handlebars')
 app.engine('handlebars', engine());
 app.set('views', path.join(__dirname, "src", "views"));
@@ -12,4 +13,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
 })
-module.exports = app; 
+module.exports = app;
