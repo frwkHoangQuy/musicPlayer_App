84d740104c69ae9d31d413046f538768b0a8df40
Quy <frwk.HoangQuy@gmail.com>
2023-06-14 12:17:35 +0700

    Create HomePage's HTML

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {engine} = require("express-handlebars");
+const path = require('path')
+const app = express(); 
+const port = 3000; 
+app.set('view engine', 'handlebars')
+app.engine('handlebars', engine());
+app.set('views', path.join(__dirname, "src", "views"));
+app.get('/', (req, res) => {
+  res.render('home/home');
+});
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
+module.exports = app; 
